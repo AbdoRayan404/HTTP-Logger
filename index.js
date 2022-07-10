f1867f01cbad05e88d9802a340206f3757e6f807
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-10 12:58:09 +0200

    simple PUT support

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ function httpLogger(req, res, next){
         if(process.env.LOG_BODY == 'true') process.stdout.write(`[BODY] ${JSON.stringify(req.body)} `)
         if(process.env.LOG_AUTHORIZATION == 'true') process.stdout.write(`[AUTH] ${req.headers['authorization'] ? req.headers['authorization'] : "No Auth token were sent"}`)
         console.log('')
-    }else if(req.method == "DELETE"){
+    }
+    else if(req.method == "DELETE"){
         process.stdout.write(`[${req.protocol.toUpperCase()}/${req.httpVersion}] ${req.method} '${req.originalUrl}' \x1b[32m${req.ip}\x1b[0m ${new Date().toDateString()} `)
         process.stdout.write(`[REQUEST] '${req.headers['user-agent']}' encoding:'${req.headers['accept-encoding']}' `)
+        if(process.env.LOG_AUTHORIZATION == 'true') process.stdout.write(`[AUTH] ${req.headers['authorization'] ? req.headers['authorization'] : "No Auth token were sent"}`)
+        console.log('')
+    }
+    else if(req.method == "PUT"){
+        console.log(req)
+        process.stdout.write(`[${req.protocol.toUpperCase()}/${req.httpVersion}] ${req.method} '${req.originalUrl}' \x1b[32m${req.ip}\x1b[0m ${new Date().toDateString()} `)
+        process.stdout.write(`[REQUEST] '${req.headers['user-agent']}' encoding:'${req.headers['accept-encoding']}' `)
+        if(process.env.LOG_BODY == 'true') process.stdout.write(`[BODY] ${JSON.stringify(req.body)} `)
         if(process.env.LOG_AUTHORIZATION == 'true') process.stdout.write(`[AUTH] ${req.headers['authorization'] ? req.headers['authorization'] : "No Auth token were sent"}`)
         console.log('')
     }
